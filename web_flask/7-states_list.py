76b55d7aa2ce49917c5bb6bbe99db1bfa7dbd095
Shadkoech <shadrackipruto@gmail.com>
2023-12-31 11:46:17 +0300

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,21 +14,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/states_list", strict_slashes=False)
-def list_states:
-    """Route handler for /states_list
-    obtains all the states created in storage
-    """
-    states = storage.all("states")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontent
-def close_db:
+@app.teardown_appcontext
+def close_db(error):
     """Closes and removes the running SQLAlchemy session
     """
     storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """Route handler for /states_list
+    obtains all the states created in storage
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
